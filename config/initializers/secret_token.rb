dd4cd309439345702f45e6455f8df2d42b78b80f
metalhardy <hardy.lim@maxusglobal.com>
2014-01-08 16:43:19 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Metalbaby::Application.config.secret_key_base = '63be9b5dd69b6891bcab46b16401c8d26edb62d1530b5a3d88b5fdaa9008364c7d2220c92fc6201cf86c92afb9090b122c4ff58ce3417ad31ce50edf1eca44b6'
